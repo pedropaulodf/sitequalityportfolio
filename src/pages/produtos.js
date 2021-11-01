a14a9637d9e07387fe2c9c0fd0d4c327ea706519
pedropaulodf <pedropaulotj@gmail.com>
2021-11-01 17:33:47 -0300

    backup

--- src/pages/produtos.js
+++ src/pages/produtos.js
@@ -1,10 +1,19 @@
+import Head from "next/head";
 import { Footer } from "../components/Footer";
 
 export default function Produtos(){
 
   return (
-    <div>
+    <>
+      <Head>
+        <title>Produtos | Quality Systems</title>
+        <meta
+          name="description"
+          content="Desde a sua fundação em 1990, a Quality Systems vem oferecendo soluções inovadoras para gestão e automação de clubes, agremiações, sindicatos e academias."
+        />
+      </Head>
+      
       <Footer/>
-    </div>
+    </>
   )
 }
