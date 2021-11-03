d44c9f404350624b47de43e2029d29c400523b9c
pedropaulodf <pedropaulotj@gmail.com>
2021-11-03 17:37:08 -0300

    backup

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,18 +1,51 @@
 import Link from 'next/link';
+import { useEffect, useState } from 'react';
 import { FiUser } from "react-icons/fi";
 
 import styles from "./styles.module.scss";
 
 export function Header() {
+  
+  const [menuSelecionado, setMenuSelecionado] = useState({
+    inicio: true,
+    sobre: false,
+    produtos: false,
+    contato: false,
+  });
+
+  useEffect(() => {
+    switch (window.location.pathname) {
+      case '/':
+        setMenuSelecionado({...menuSelecionado, inicio: true});
+        break;
+    
+      case '/sobre':
+        setMenuSelecionado({...menuSelecionado, sobre: true});
+        break;
+    
+      case '/produtos':
+        setMenuSelecionado({...menuSelecionado, produtos: true});
+        break;
+    
+      case '/contato':
+        setMenuSelecionado({...menuSelecionado, contato: true});
+        break;
+    
+      default:
+        setMenuSelecionado({...menuSelecionado, inicio: true});
+        break;
+    }
+  },[])
+
   return (
     <header className={styles.container}>
       <div className={styles.wrapper}>
         <img src="/images/logo.png" alt="Logo Quality Systems" />
         <nav>
-          <Link href="/" ><a className={styles.active}>Início</a></Link>
-          <Link href="/sobre" ><a>Sobre a Quality</a></Link>
-          <Link href="/produtos" ><a>Produtos</a></Link>
-          <Link href="/contato" ><a>Contato</a></Link>
+          <Link href="/" ><a className={menuSelecionado.inicio && styles.active}>Início</a></Link>
+          <Link href="/sobre" ><a className={menuSelecionado.sobre && styles.active}>Sobre a Quality</a></Link>
+          <Link href="/produtos" ><a className={menuSelecionado.produtos && styles.active}>Produtos</a></Link>
+          <Link href="/contato" ><a className={menuSelecionado.contato && styles.active}>Contato</a></Link>
         </nav>
         {/* <SignInButton/> */}
         <button type="button">
