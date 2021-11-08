8918ba57cef8b02f2fbe950b50c74c60e5c7576d
pedropaulodf <pedropaulotj@gmail.com>
2021-11-08 16:48:11 -0300

    backup

---
+++ src/pages/privacidade.js
@@ -0,0 +1,19 @@
+import Head from "next/head";
+import { Footer } from "../components/Footer";
+
+export default function Privacidade(){
+
+  return (
+    <>
+      <Head>
+        <title>Privacidade | Quality Systems</title>
+        <meta
+          name="description"
+          content="Desde a sua fundação em 1990, a Quality Systems vem oferecendo soluções inovadoras para gestão e automação de clubes, agremiações, sindicatos e academias."
+        />
+      </Head>
+
+      <Footer/>
+    </>
+  )
+}
